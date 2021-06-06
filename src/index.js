6fa3b144fca0a6b32d8ed272cf03520838393551
Craig Miller <craigmiller160@gmail.com>
2021-06-06 13:47:00 -0400

    Finalized script

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 const fs = require('fs');
 
-const pathToDirOld = '/home/craig/MediaDrive/Media/Movies/Apollo 13 (1995, PG-13)';
-const movieFile = 'Apollo.13.1995.1080p.BluRay.x264.YIFY.mp4';
-const srtFile = 'Apollo.13.1995.1080p.BluRay.x264.YIFY.srt';
-
 const pathToDir = '/home/craig/OtherDrive/New folder 1'
-
-const checkDirAccessOld = () => {
-	const files = fs.readdirSync(pathToDirOld);
-	if (files.length !== 2) {
-		throw new Error(`No files in directory: ${pathToDirOld}`);
-	}
-
-	files.sort((a,b) => a.localeCompare(b));
-	if (movieFile !== files[0]) {
-		throw new Error(`Invalid first file: Expected: ${movieFile} Found: ${files[0]}`);
-	}
-	if (srtFile !== files[1]) {
-		throw new Error(`Invalid second file: Expected: ${srtFile} Found: ${files[1]}`);
-	}
-};
+const maxErrors = 20;
 
 const checkDirAccess = () => {
 	const files = fs.readdirSync(pathToDir);
@@ -29,11 +11,17 @@ const checkDirAccess = () => {
 };
 
 console.log('Running directory check');
+let counter = 0;
 
-setInterval(() => {
+const interval = setInterval(() => {
 	try {
 		checkDirAccess();
 	} catch (ex) {
-		console.error(ex);
+		counter++;
+		console.error(`Error at ${new Date()}: ${ex.message}`);
+		if (counter === maxErrors) {
+			clearInterval(interval);
+			console.error('Exceeded maximum errors, discontinuing execution');
+		}
 	}
 }, 500);
