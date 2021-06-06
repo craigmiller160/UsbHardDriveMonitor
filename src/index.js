5a1376f1c43d6fe5a67c2f98d3209375d8c72a91
Craig Miller <craigmiller160@gmail.com>
2021-06-06 13:39:49 -0400

    Making progress with logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 const fs = require('fs');
 
-const pathToDir = '/home/craig/MediaDrive/Media/Movies/Apollo 13 (1995, PG-13)';
+const pathToDirOld = '/home/craig/MediaDrive/Media/Movies/Apollo 13 (1995, PG-13)';
+const movieFile = 'Apollo.13.1995.1080p.BluRay.x264.YIFY.mp4';
+const srtFile = 'Apollo.13.1995.1080p.BluRay.x264.YIFY.srt';
+
+const pathToDir = '/home/craig/OtherDrive/New folder 1'
+
+const checkDirAccessOld = () => {
+	const files = fs.readdirSync(pathToDirOld);
+	if (files.length !== 2) {
+		throw new Error(`No files in directory: ${pathToDirOld}`);
+	}
+
+	files.sort((a,b) => a.localeCompare(b));
+	if (movieFile !== files[0]) {
+		throw new Error(`Invalid first file: Expected: ${movieFile} Found: ${files[0]}`);
+	}
+	if (srtFile !== files[1]) {
+		throw new Error(`Invalid second file: Expected: ${srtFile} Found: ${files[1]}`);
+	}
+};
 
 const checkDirAccess = () => {
 	const files = fs.readdirSync(pathToDir);
-	console.log(files);
+	if (files.length === 0) {
+		throw new Error(`No files in directory: ${pathToDir}`);
+	}
 };
 
-checkDirAccess();
+console.log('Running directory check');
+
+setInterval(() => {
+	try {
+		checkDirAccess();
+	} catch (ex) {
+		console.error(ex);
+	}
+}, 500);
